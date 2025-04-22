5a08401534374316c8820d921c17208bb8c978da
zaheer122 <90451604+zaheer122@users.noreply.github.com>
2025-04-22 21:45:41 +0530

    Delete TimeSlot.kt

--- TimeSlot.kt
+++
@@ -1,9 +0,0 @@
-// TimeSlot.kt
-package com.app.medifindfinal
-
-data class TimeSlot(
-    val time: String? = null,
-    val isAvailable: Boolean = true,
-    val clinicId: String? = null
-    // Add other relevant fields
-)
